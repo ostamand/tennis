4a037dbe0fae09f02c399f5dcfdd30759f5ea650
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2018-11-11 11:42:45 -0500

    feat: Add tensorboard for training logging

--- ddpg/train_agent.py
+++ ddpg/train_agent.py
@@ -1,10 +1,12 @@
 # pylint: skip-file
 from collections import deque
 import numpy as np
+from tensorboardX import SummaryWriter
 
 def train(env, agent, episodes=1000, steps=200, 
           log_each=10, save_thresh=90.0, save_file='saved_models/ddpg.ckpt'):
     scores = deque(maxlen=100)
+    writer = SummaryWriter()
     last_saved = 0
     for ep_i in range(episodes):
         state = env.reset()
@@ -18,12 +20,14 @@ def train(env, agent, episodes=1000, steps=200,
             state = next_state
             if done: 
                 break
+        writer.add_scalar('data/reward', score, ep_i)
         scores.append(score)
 
         summary = f'Episode: {ep_i+1}/{episodes}, Reward: {score:.2f}'
         if len(scores) >= 100:
             mean = np.mean(scores)
             summary += f', Score: {mean:.2f}'
+            writer.add_scalar('data/score', mean, ep_i)
 
             if mean > save_thresh and mean > last_saved:
                 summary += ' (saved)'
