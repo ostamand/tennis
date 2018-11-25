70c7b063a570e2a474e7f3f37f70ec69fd30052b
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2018-11-25 14:57:20 -0500

    fix: Correct watch train tennis agent

--- watch_trained_tennis.py
+++ watch_trained_tennis.py
@@ -4,28 +4,33 @@ from tennis.model import Actor, Critic
 from unity_env import UnityEnv
 import numpy as np
 
-def watch(env, agent):
+def watch(env, agent, episodes):
     state = env.reset(train=False)
-    score = np.zeros(2,2)
-    while True:
-        action = agent.act(state.reshape(-1), learn=False)
-        next_state, reward, done = env.step(action.reshape(2, -1))
-        score += reward
-        state = next_state
-        if done.any():
-            break
-    return np.max(score)
+    scores = []
+    for ep_i in range(episodes):
+        score = np.zeros((2,2))
+        while True:
+            action = agent.act(state.reshape(-1), learn=False)
+            next_state, reward, done = env.step(action.reshape(2, -1))
+            score += reward
+            state = next_state
+            if done.any():
+                break
+        scores.append(np.max(score))
+    return scores
 
 if __name__ == '__main__':
     # create environment
     env = UnityEnv(no_graphics=False)
     state_size = env.state_size*2
-    action_size = env.action_shape*2 
+    action_size = env.action_size*2 
 
     # restore agent checkpoint
     agent = Agent(state_size, action_size, Actor, Critic, restore='saved_models/tennis_ddpg.ckpt')
 
     # watch agent 
-    score = watch(env, agent)
+    scores = watch(env, agent, 10)
 
-    print(f'Episode score: {score:.2f}')
+    print(f'Average score over 10 episodes: {np.mean(scores):.2f}')
+
+    env.close()
